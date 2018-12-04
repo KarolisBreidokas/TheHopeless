98696fb0a2372a74a2e1869d4f6ef9b76d3c4787
Montekis <Montekis@users.noreply.github.com>
2018-12-04 03:49:55 +0200

    update

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let classCol= document.getElementsByClassName('col');
 let classCol6 = document.getElementsByClassName('col-6');
+let i = 0;
+
 
 let myFunction = function(){
+    if(this.id=='catalogView2'){
+    let source = document.getElementById('catalogImage');
+    document.getElementById('orderedImage').src = source.src;
+    }
+    else{
     console.log(this.id);
     location.replace('./html/'+this.id+'.html');
+    }
 };
 
 window.onload = function(){
@@ -12,11 +20,42 @@ for( let i=0; i < classCol.length; i++){
 }
 
 for( let i=0; i < classCol6.length; i++){
+    if(classCol6[i].id!='catalogView3' && classCol6[i].id!='catalogView4' && classCol6[i].id!='catalogView')
     classCol6[i].addEventListener('click', myFunction, false);
 }
+let buttonNext = document.getElementById('buttonNext');
+let buttonPrevious = document.getElementById('buttonPrevious');
+let image = document.getElementById('catalogImage');
+this.addListeners();
 
 }
 
-function sayHello(){
-    console.log('hello');
+function addListeners(){
+    buttonNext.addEventListener('click', buttonPressed, false);
+    buttonPrevious.addEventListener('click', buttonPressed, false);
+}
+
+function buttonPressed(){
+    if(this.id == 'buttonNext'){
+        if(i<6){
+        i++;
+        document.getElementById('catalogImage').src='./images/'+i+'.jpg';
+        }
+        else{
+            document.getElementById('catalogImage').src='./images/'+0+'.jpg'; 
+            i=0;  
+        }
+    }
+    else{
+        if(this.id=='buttonPrevious'){
+        if(i>0){
+        i--;
+        document.getElementById('catalogImage').src='./images/'+i+'.jpg';
+        }
+        else{
+            document.getElementById('catalogImage').src='./images/'+6+'.jpg';
+            i=6;
+        }
+        }
+    }
 }
