186c62bf09cdd60a89c04938ca555de2cd3ab7a1
Karolis Breidokas <karolis.breidokas@ktu.edu>
2018-12-06 17:42:55 +0200

    Adding AutoMapper

---
+++ TheHopeless/TheHopeless.API/Configurations/AutomapperConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using TheHopeless.API.Contracts.ProductController;
+using TheHopeless.API.Database.Entities.ProductControl;
+using Attribute = TheHopeless.API.Database.Entities.ProductControl.Attribute;
+
+namespace TheHopeless.API.Configurations
+{
+    public class AutomapperConfiguration:Profile
+    {
+        public AutomapperConfiguration() : this("AcademyDemo")
+        {
+
+        }
+        protected AutomapperConfiguration(string name) : base(name)
+        {
+            CreateMap<Product, ProductDto>(MemberList.None)
+                .ForMember(x => x.Attributes, opt => opt.MapFrom(y => y.Values))
+                .ForMember(x => x.Picture,
+                    opt => opt.MapFrom(y => y.Pictures.Select(z => new PictureDto() {PictureId = z.Id})));
+            CreateMap<ProductAttribute, ProductAttributeDto>(MemberList.None);
+            CreateMap<Attribute, AttributeDto>(MemberList.None);
+        }
+
+
+    }
+}
