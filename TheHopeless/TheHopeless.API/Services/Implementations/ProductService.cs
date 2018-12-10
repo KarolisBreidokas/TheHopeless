1d63a2e65e56f6bd3f664b98e4eb7775b4e863f8
Karolis Breidokas <karolis.breidokas@ktu.edu>
2018-12-10 19:52:37 +0200

    finishing up Product service

--- TheHopeless/TheHopeless.API/Services/Implementations/ProductService.cs
+++ TheHopeless/TheHopeless.API/Services/Implementations/ProductService.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
 using TheHopeless.API.Contracts.ProductController;
 using TheHopeless.API.Database;
 using TheHopeless.API.Database.Entities.ProductControl;
 
+
+
 namespace TheHopeless.API.Services.Implementations
 {
-    public class ProductService:IProductService
+    public class ProductService : IProductService
     {
         private readonly EshopDbContext _context;
         private readonly DbSet<Product> _products;
@@ -22,33 +24,65 @@ namespace TheHopeless.API.Services.Implementations
             _products = context.Products;
         }
 
+        private IQueryable includeDependencies(IQueryable set)
+        {
+            return set.
+        }
         public async Task<ICollection<ProductDto>> Get()
         {
-            var ans=await _products.Include(x=>x.Values).ThenInclude(x=>x.Attribute).Include(x=>x.Pictures).ToListAsync();
+            var ans = await IncludeDependencies().ToListAsync();
             return _mapper.Map<ICollection<ProductDto>>(ans);
+        }
+
+        private IQueryable<Product> IncludeDependencies()
+        {
+            return _products.Include(x => x.Values).ThenInclude(x => x.Attribute).Include(x => x.Pictures);
         }
 
         public async Task<ProductDto> GetSpecific(int id)
         {
 
-            var ans = await _products.Include(x => x.Values).ThenInclude(x => x.Attribute).Include(x => x.Pictures)
-                .Where(x => x.Id == id).FirstOrDefaultAsync();
+            var ans = await IncludeDependencies().Where(x => x.Id == id).FirstOrDefaultAsync();
             return _mapper.Map<ProductDto>(ans);
         }
 
-        public Task<ProductDto> Add(NewProductDto newProduct)
+        public async Task<ProductDto> Add(NewProductDto newProduct)
         {
-            throw new NotImplementedException();
+            var entity = _mapper.Map<Product>(newProduct);
+            _products.Add(entity);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductDto>(entity); ;
+
         }
 
-        public Task<ProductDto> Edit(int id, NewProductDto product)
+        public async Task<ProductDto> Edit(int id, NewProductDto product)
         {
-            throw new NotImplementedException();
+            var entity = await IncludeDependencies().Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (entity is null)
+            {
+                return null;
+            }
+
+            _context.Update(entity);
+            _mapper.Map(product, entity);
+            
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductDto>(entity);
         }
 
-        public Task<bool> RemoveFromSale(int id)
+        public async Task<bool> RemoveFromSale(int id)
         {
-            throw new NotImplementedException();
+            var entity = await IncludeDependencies().Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (entity is null)
+            {
+                return false;
+            }
+            
+            _context.Update(entity);
+            entity.Sellable = false;
+            
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
